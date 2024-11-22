42c97e306bc8006fe9238f0bc6fde228aa9dea70
Diogo <moraeslimadiogo@gmail.com>
2024-11-22 10:57:53 -0300

    Bug Fixes. Animação background e adição de icons.

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ const navMenu = document.getElementById('navMenu');
 const navbar = document.getElementById('navbar');
 const bemvindo = document.getElementById('bemvindo');
 const background = document.getElementById('background');
-const content = document.getElementById('content');
-const stopScroll = 100; // Adjust this value to change when the background fixes
+const sobreMim = document.getElementById('sobreMim');
+const foto = document.getElementById('foto');
+const cards = document.getElementById('cards');
+
+const stopScroll = 100; 
 
 menuToggle.addEventListener('click', () => {
     navMenu.classList.toggle('hidden');
@@ -14,18 +17,32 @@ window.addEventListener('scroll', () => {
     const scrollPosition = window.scrollY;
 
     if (scrollPosition >= stopScroll) {
-        navbar.classList.remove('bg-transparent');
+
         background.style.position = 'fixed';
         background.style.height = '7%'
         background.style.zIndex = '39'
         bemvindo.style.opacity = '0'
+        cards.style.opacity = '100'
+        foto.style.opacity = '100'
+        lucide.createIcons();
  
     } else {
-        navbar.classList.add('bg-transparent');
-        background.style.position = 'fixed';
+
+        background.style.position = 'absolute';
         background.style.top = '0';
+        background.style.height = '70%'
+        bemvindo.style.opacity = '100'
+        cards.style.opacity = '0'
+        foto.style.opacity = '0'
+
     }
 
+    const maxDistance = 100; // Distância máxima em pixels
+    const distance = Math.min(scrollPosition / 2, maxDistance);
+    sobreMim.style.transform = `translateY(${distance}px)`;
 
+    
 });
+
+lucide.createIcons();
 
